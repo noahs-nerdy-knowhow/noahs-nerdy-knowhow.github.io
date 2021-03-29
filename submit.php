e8ec56a8c26eea0c8f963c013a8bea2ee758a1fe
noahs-nerdy-knowhow <78937679+noahs-nerdy-knowhow@users.noreply.github.com>
2021-03-29 01:47:46 -0500

    Create submit.php

---
+++ submit.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title>Submitted</title>
+  </head>
+  <body>
+    <?php
+    echo "Your entry has been submitted. You may safely close this page."
+    ?>
+  </body>
+</html>
